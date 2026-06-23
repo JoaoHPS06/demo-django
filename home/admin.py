3b7981d2552b2840fac2e770ca55506c6c9e41da
JoaoHPS06 <joao.hps@aluno.ufop.edu.br>
2026-06-23 15:23:11 -0300

    Roteiro completo parte 4

--- home/admin.py
+++ home/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Categoria, Mensagem
 
-@admin.register(Categoria)                                      
+from .models import Categoria, Mensagem, Tag
+
+
+@admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
+
+
+@admin.register(Tag)                                            
+class TagAdmin(admin.ModelAdmin):
     list_display = ("nome",)
     search_fields = ("nome",)
 
 
 @admin.register(Mensagem)
 class MensagemAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "categoria", "criada_em")        
-    list_filter = ("categoria",)                                
+    list_display = ("titulo", "categoria", "criada_em")
+    list_filter = ("categoria", "tags")                         
     search_fields = ("titulo", "conteudo")
+    filter_horizontal = ("tags",)                              
--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class Categoria(models.Model):                               
+
+class Categoria(models.Model):
     nome = models.CharField(max_length=50, unique=True)
 
     class Meta:
@@ -9,14 +10,30 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nome
 
+
+class Tag(models.Model):                                       
+    nome = models.SlugField(max_length=30, unique=True)
+
+    class Meta:
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
+
+
 class Mensagem(models.Model):
     titulo = models.CharField(max_length=120)
     conteudo = models.TextField()
-    autor = models.CharField(max_length=80, default="Anônimo") 
-    categoria = models.ForeignKey(                              
+    autor = models.CharField(max_length=80, default="Anônimo")
+    categoria = models.ForeignKey(
         Categoria,
         on_delete=models.SET_NULL,
         null=True,
+        blank=True,
+        related_name="mensagens",
+    )
+    tags = models.ManyToManyField(                             
+        Tag,
         blank=True,
         related_name="mensagens",
     )
@@ -28,4 +45,4 @@ class Mensagem(models.Model):
         ordering = ["-criada_em"]
 
     def __str__(self):
-        return self.titulo 
+        return self.titulo
